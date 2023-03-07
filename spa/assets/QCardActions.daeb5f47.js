de8d5ad3da1ef2578623d12c1ad5dcb6e027ddd6
Chris Mac <cjw@marlboroughcollege.org>
2023-03-07 08:32:44 +0000

    updated dates page. Holidays now display.

---
+++ spa/assets/QCardActions.daeb5f47.js
@@ -0,0 +1,1 @@
+import{u as t,au as r,av as c,d as n,h as l,z as i}from"./index.d8108594.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
